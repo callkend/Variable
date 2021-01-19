97b9f0c84be8f9bb1be4a19996eba9b3a8612608
callkend <77285430+callkend@users.noreply.github.com>
2021-01-19 08:50:00 -0700

    Tuesday. Variable examples

--- VariableExample.vb
+++ VariableExample.vb
@@ -1,6 +1,34 @@
-﻿Module VariableExample
+﻿'header stuff
+
+Option Strict On 'No conversions by the compiler
+Option Explicit On 'Must declare all variables
+
+Module VariableExample 'PascalCase
 
     Sub Main()
+
+        Dim x As Int16
+        Dim y As Int16
+        Dim result As Int16
+        Dim firstName As String
+        Dim lastName As String
+
+
+        x = 1
+        y = 2
+        result = x + y
+
+
+        Console.WriteLine("What is your first name?")
+        firstName = Console.ReadLine()
+        Console.WriteLine("What is you last name?")
+        lastName = Console.ReadLine()
+        Console.WriteLine("Hello " +
+                          firstName + " " + lastName) 'wrap long text with a space underscore.
+        Console.ReadLine()
+
+
+
 
     End Sub
 
